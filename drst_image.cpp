80a33952e46135de7565ededbeab0679893d709d
Boris <82962853+borofx@users.noreply.github.com>
2026-05-06 16:14:05 +0300

    Implement DRSTImage constructor
    Add DRSTImage constructor to read DRST files

--- drst_image.cpp
+++ drst_image.cpp
@@ -1,2 +1,25 @@
-#include <iostream>
+#include "drst_image.hpp"
 #include <fstream>
+#include <stdexcept>
+
+static const int TILES = 48;
+static const int COL[] = {0,1,2,3, 0,3, 0,3, 0,1,2,3};
+static const int ROW[] = {0,0,0,0, 1,1, 2,2, 3,3,3,3};
+
+DRSTImage::DRSTImage(std::istream& in) {
+    char magic[4];
+    in.read(magic, 4);
+    if (magic[0]!='D'||magic[1]!='R'||magic[2]!='S'||magic[3]!='T')
+        throw std::runtime_error("Error: not DRST file");
+
+    in.read((char*)&N, 4);    
+    blocks = new Pixel[TILES * N * N];
+
+    for (int t = 0; t < TILES; ++t){
+        for (unsigned int i = 0; i < N*N; ++i) {
+            char b[3];
+            in.read(b, 3);
+            blocks[t*N*N + i] = {(unsigned char)b[0], (unsigned char)b[1], (unsigned char)b[2]};
+        }
+    }        
+}
