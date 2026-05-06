d859bc32ea828aac9078acd6b1b8c6a9f76a00a8
Boris <82962853+borofx@users.noreply.github.com>
2026-05-06 16:03:37 +0300

    Declare DRSTImage & Pixel classes

--- drst_image.hpp
+++ drst_image.hpp
@@ -1,1 +1,37 @@
+#pragma once
+#include <functional>
+#include <iostream>
+#include <string>
 
+struct Pixel {
+    unsigned char r, g, b;
+};
+
+struct Position {
+    double x, y;
+};
+
+class DRSTImage {
+public:
+    DRSTImage(std::istream& in);
+    unsigned int getSize() const;
+
+    Pixel getPixel(int imageIndex, int x, int y) const;
+    Pixel getPixel(double x, double y) const;
+
+    void saveAsPPM(const std::string& filename, size_t size) const;
+    void saveAsPPM(const std::string& filename, size_t size, std::function<Position(Position)> phi) const;
+    
+    ~DRSTImage();
+    DRSTImage(const DRSTImage&) = delete;
+    DRSTImage& operator=(const DRSTImage&) = delete;
+
+private:
+    unsigned int N;
+    Pixel* blocks;
+    struct BlockRect {
+        double x, y, w, h;
+    };
+    BlockRect getBlockRect(int imageIndex) const;
+    int       findTile(double x, double y) const;
+};
